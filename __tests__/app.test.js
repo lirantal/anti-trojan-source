3f0f2e57731d1d61f05765dc6c9954e246374279
Liran Tal <liran.tal@gmail.com>
2021-11-05 16:46:24 +0200

    test: more test cases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,15 @@ test('shouldnt detect dangerous because this text is innocent', () => {
   expect(hasTrojanSource({ sourceText: 'bla bla bla' })).toBe(false)
 })
 
+test('shouldnt detect dangerous because this text is innocent too even if it has some control chars', () => {
+  expect(
+    hasTrojanSource({
+      sourceText:
+        'bla \n bla \r bla \n\r more \t\t\t and more \u204c and also this greek question mark ; which isnt ;'
+    })
+  ).toBe(false)
+})
+
 test('the list of dangerous bidi characters should be more than 0', () => {
   expect(dangerousBidiChars.length).toBeGreaterThan(0)
 })
