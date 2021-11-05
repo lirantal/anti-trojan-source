6028467ad0d9b6f24b612832532d44d712f3e5b4
Liran Tal <liran.tal@gmail.com>
2021-11-05 19:28:11 +0200

    test: add test cases for files handling

---
+++ __tests__/files-detection.test.js
@@ -0,0 +1,28 @@
+import { hasTrojanSourceInFiles } from '../src/main.js'
+import path from 'path'
+
+const __dirname = new URL('.', import.meta.url).pathname
+
+test('should detect that file really has trojan source', () => {
+  const vulnerableFiles = [path.join(__dirname, '__fixtures__', 'true-trojan-source.js')]
+
+  const result = hasTrojanSourceInFiles({ filePaths: vulnerableFiles })
+  expect(result).toHaveLength(1)
+  expect(result[0].file.includes('true-trojan-source.js')).toBe(true)
+})
+
+test('should not detect that file really has trojan source because it really doesnt have it', () => {
+  const vulnerableFiles = [path.join(__dirname, '__fixtures__', 'false-trojan-source.js')]
+
+  const result = hasTrojanSourceInFiles({ filePaths: vulnerableFiles })
+  expect(result).toHaveLength(0)
+})
+
+test('should not detect that file really has trojan source because when file paths dont actually exist', () => {
+  const vulnerableFiles = [
+    path.join(__dirname, '__fixtures__', 'this-doesnt-really-exist-on-the-fs-trojan-source.js')
+  ]
+
+  const result = hasTrojanSourceInFiles({ filePaths: vulnerableFiles })
+  expect(result).toHaveLength(0)
+})
